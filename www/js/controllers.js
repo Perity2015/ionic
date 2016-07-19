ad6414b23241caad69fe1068847b7a90662a1f3a
Perity2015 <757345733@qq.com>
2016-07-19 18:18:31 +0800

    18:18:22

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,9 +1,12 @@
 angular.module('starter.controllers', [])
+  .controller(function ($scope, $ionicPopup, $timeout) {
+
+  })
 
   .controller('DashCtrl', function ($scope) {
   })
 
-  .controller('ChatsCtrl', function ($scope, Chats) {
+  .controller('ChatsCtrl', function ($scope, $http, $location, $filter, Chats) {
     // With the new view caching in Ionic, Controllers are only called
     // when they are recreated or on app start, instead of every page change.
     // To listen for when this page is active (for example, to refresh data),
@@ -12,21 +15,90 @@ angular.module('starter.controllers', [])
     //$scope.$on('$ionicView.enter', function(e) {
     //});
 
+
     $scope.chats = Chats.all();
     $scope.remove = function (chat) {
       Chats.remove(chat);
     };
+
+    $scope.doRefresh = function () {
+      var promise = $http({
+        method: 'POST',
+        url: '/OnceRfid.BaseOnceRfid/GetLinkGoodRecord_RawJson.ajax',
+        data: {page: '1', rows: '10'}
+      });
+      promise.then(function (resp) {
+        console.info(resp.data.rows);
+        if (resp.headers("sessionStatus")) {
+          $location.path('/tab/account');
+          return;
+        }
+        Chats.updateTotal(resp.data.total);
+        Chats.update(resp.data.rows, $filter);
+        $scope.chats = Chats.all();
+
+      }, function (resp) {
+
+      }).finally(function () {
+        // 停止广播ion-refresher
+        $scope.$broadcast('scroll.refreshComplete');
+      })
+    };
+
+    $scope.LoadMore = function () {
+      var promise = $http({
+        method: 'POST',
+        url: '/OnceRfid.BaseOnceRfid/GetLinkGoodRecord_RawJson.ajax',
+        data: {page: Chats.length() + 1, rows: '10'}
+      });
+
+      promise.then(function (resp) {
+        // console.info(resp.data.rows);
+        if (resp.headers("sessionStatus")) {
+          $location.path('/tab/account');
+          return;
+        }
+        Chats.updateTotal(resp.data.total);
+        Chats.add(resp.data.rows, $filter);
+        $scope.chats = Chats.all();
+      }, function (resp) {
+
+      }).finally(function () {
+        $scope.$broadcast('scroll.infiniteScrollComplete');
+      })
+    };
+
+    $scope.$on('stateChangeSuccess', function () {
+      $scope.LoadMore();
+    });
+
+    $scope.canLoadMore = function () {
+      return Chats.canLoadMore();
+    }
   })
 
   .controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
     $scope.chat = Chats.get($stateParams.chatId);
+    console.info($scope.chat);
+    $scope.$on('$ionicView.afterEnter', function () {
+      var map = new AMap.Map('mapContainer', {
+        center: [121.498586, 31.239637]
+      });
+    }, false);
+
   })
 
-  .controller('AccountCtrl', function ($scope, $ionicModal, $ionicPopover, $timeout, $location, $ionicPopup, $http) {
+  .controller('AccountCtrl', function ($scope, $stateParams, $ionicModal, $ionicHistory, $ionicPopover, $timeout, $location, $ionicPopup, $http, Account) {
+    $scope.user = Account.instance();
+
     $scope.login = function (user) {
-      if (undefined == user) {
+      if (undefined == user || user.username == '' || user.password == '') {
+        $scope.showAlert('请输入完整信息');
         return
       }
+
+      Account.update(user);
+
       var promise = $http({
         method: 'POST',
         url: '/CoreSYS.SYS/LGKeyLogin.index.ajax',
@@ -37,7 +109,11 @@ angular.module('starter.controllers', [])
           $scope.showAlert(resp.data.sMsg);
           return;
         }
-        $location.path('/tab/account');
+        var tempUserInfo = resp.data.m_ReturnOBJ.m_UserInfo;
+        Account.updateUserInfo(tempUserInfo)
+        // $location.path('/tab/account');
+        console.info(resp.headers("sessionStatus"));
+        $ionicHistory.goBack();
       }, function (resp) {
 
       });
@@ -46,7 +122,8 @@ angular.module('starter.controllers', [])
     $scope.showAlert = function (msg) {
       var alertPopup = $ionicPopup.alert({
         title: '提示',
-        template: msg
+        template: msg,
+        buttons: [{text: '确认', type: 'button-positive'}]
       });
-    };
+    }
   });
--- www/js/services.js
+++ www/js/services.js
@@ -1,50 +1,121 @@
 angular.module('starter.services', [])
 
-.factory('Chats', function() {
-  // Might use a resource here that returns a JSON array
-
-  // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/ben.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
-  }];
-
-  return {
-    all: function() {
-      return chats;
-    },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
-        }
+  .filter("jsonDate", function ($filter) {
+    return function (input, format) {
+      if (input == null) {
+        return null;
       }
-      return null;
+
+      //从字符串 /Date(1448864369815)/ 得到时间戳 1448864369815
+      var timestamp = Number(input.replace(/\/Date\((\d+)\)\//, "$1"));
+
+      //转成指定格式
+      return $filter("date")(timestamp, format);
+    };
+  })
+
+  .factory('Chats', function () {
+    // Might use a resource here that returns a JSON array
+    var chat = {
+      beginaddr: null,
+      beginorgna_id: null,
+      beginorgna_name: null,
+      begintime: null,
+      beginuser: null,
+      beginusername: null,
+      bok: true,
+      bover: false,
+      company: null,
+      companyid: null,
+      deskaddr: null,
+      endaddr: null,
+      endorgna_id: 0,
+      endorgna_name: null,
+      endtime: null,
+      enduser: null,
+      endusername: null,
+      errormsg: null,
+      flowid: 33,
+      flowname: null,
+      id: 16322,
+      linkrfid: null,
+      linkgoodname: null,
+      overpaperpic: null,
+      papermemo: null,
+      beginlat: null,
+      beginlng: null,
+      endlat: null,
+      endlng: null
+    };
+    // Some fake testing data
+    var chats = [];
+    var total = 0;
+
+    return {
+      all: function () {
+        return chats;
+      },
+      remove: function (chat) {
+        chats.splice(chats.indexOf(chat), 1);
+      },
+      update: function (items, $filter) {
+        for (var i = 0; i < items.length; i++) {
+          items[i].begintime = $filter("jsonDate")(items[i].begintime, "yyyy-MM-dd HH:mm:ss");
+          items[i].endtime = $filter("jsonDate")(items[i].endtime, "yyyy-MM-dd HH:mm:ss");
+          chats.push(items[i]);
+        }
+      },
+      get: function (chatId) {
+        for (var i = 0; i < chats.length; i++) {
+          if (chats[i].id === parseInt(chatId)) {
+            return chats[i];
+          }
+        }
+        return null;
+      },
+      add: function (items, $filter) {
+        for (var i = 0; i < items.length; i++) {
+          items[i].begintime = $filter("jsonDate")(items[i].begintime, "yyyy-MM-dd HH:mm:ss");
+          items[i].endtime = $filter("jsonDate")(items[i].endtime, "yyyy-MM-dd HH:mm:ss");
+          chats.push(items[i]);
+        }
+      },
+      updateTotal: function (tempTotal) {
+        total = tempTotal;
+      },
+      canLoadMore: function () {
+        return total > chats.length;
+      },
+      length: function () {
+        return chats.length / 10;
+      }
     }
-  };
-});
+      ;
+  })
+
+  .factory('Account', function () {
+    var user = {
+      username: 'yc',
+      password: '0'
+    };
+
+    var userInfo = {};
+
+    return {
+      instance: function () {
+        return user;
+      },
+
+      update: function (tempUser) {
+        user = tempUser;
+      },
+
+      instanceUserInfo: function () {
+        return userInfo;
+      },
+
+      updateUserInfo: function (tempUserInfo) {
+        userInfo = tempUserInfo;
+      }
+    };
+  });
